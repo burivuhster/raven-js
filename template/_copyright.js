96b6b9fa905203871ec936fe25c30b180c736821
Eugene Molodkin <burivuh@gmail.com>
2013-06-19 14:23:39 +0400

    Add warning comment

--- template/_copyright.js
+++ template/_copyright.js
@@ -1,8 +1,10 @@
-/*! Raven.js @VERSION | github.com/getsentry/raven-js */
+/*! Raven.js @VERSION | github.com/burivuhster/raven-js */
+
+/*! WARNING!!! It is not original raven.js, but it's fork by burivuh */
 
 /*
  * Includes TraceKit
- * https://github.com/getsentry/TraceKit
+ * https://github.com/burivuhster/TraceKit
  *
  * Copyright 2013 Matt Robenolt and other contributors
  * Released under the BSD license
